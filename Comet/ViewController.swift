02ed1caa182f6268a66aee149057fbb00d296c3c
Stuart Wallace <skwallace36@gmail.com>
2022-04-29 20:53:15 -0400

    Initial Commit

---
+++ Comet/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Comet
+//
+//  Created by Stuart Wallace on 4/29/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
